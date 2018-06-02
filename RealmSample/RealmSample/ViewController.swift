6d56e0f587c5b3be97a1df99ab1ad781d84c7e6b
梅津吉雅 <umetsu.yoshinori.uv4@is.naist.jp>
2018-06-02 21:30:54 +0900

    いろいろ変えた

---
+++ RealmSample/RealmSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmSample
+//
+//  Created by 梅津吉雅 on 2018/06/02.
+//  Copyright © 2018年 梅津吉雅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
